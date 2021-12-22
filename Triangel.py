6f60a6070ff6b5743148cedb0c82c123e0c8854a
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-12-22 23:05:26 +0530

    Add files via upload

---
+++ Triangel.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+canvas = turtle.Turtle()
+
+canvas.forward(150) # draw base
+
+canvas.left(120)
+
+canvas.forward(150)
+
+canvas.left(120)
+
+canvas.forward(150)
+
+turtle.done()
